6f18453c95918f62c30ac4862c33fdc10e5f6dcc
Samuel Tschiedel <mukaobr@gmail.com>
2023-05-06 13:11:27 -0300

    java-classpath-hook: don't set empty CLASSPATH
    The statement exports an empty value when there are no jars found:
    
    ```shellHook
    $> set | grep UNSET_VAR
    > # nothing
    
    $> cat << EOF > hook.sh
    export UNSET_VAR
    EOF
    
    $> . ./hook.sh
    
    $> set | grep UNSET_VAR
    > UNSET_VAR=''
    ```
    
    As a side-effect, `CLASSPATH` is no longer considered `unset` by
    operators such as `make`'s `?=`
    
    Since 7a77629490189757cf53972181c7aac1bd6a5257, the hook now handles an
    unset `CLASSPATH`, making the removed statement redundant anyway.

--- pkgs/build-support/setup-hooks/set-java-classpath.sh
+++ pkgs/build-support/setup-hooks/set-java-classpath.sh
@@ -1,8 +1,6 @@
 # This setup hook adds every JAR in the share/java subdirectories of
 # the build inputs to $CLASSPATH.
 
-export CLASSPATH
-
 addPkgToClassPath () {
     local jar
     for jar in $1/share/java/*.jar; do
